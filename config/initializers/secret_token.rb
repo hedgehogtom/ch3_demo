05382f6d06a72bd34b90478f7253c3c00603f72b
hedgehogtom <hedgehogtom@gmail.com>
2012-09-04 16:56:24 -0400

    Initial commit: empty

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ch3Demo::Application.config.secret_token = 'fd3df7932724fd4de980b8bc042984ba9a401f9ac9aaa3ed361ee8b6fe096c829d0b466692066af208253984a4188a909d76aaf0c28ad5d82472084bac85a197'
